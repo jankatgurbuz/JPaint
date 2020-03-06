9bef34f9b4f0b98e003339748851a756e56f8b5e
jankatgurbuz <jankatgurbuz@gmail.com>
2020-03-06 17:49:40 +0300

    Added Initialize Script

---
+++ Assets/Scripts/JPaint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JLibrary.Paint
+{
+    public class JPaint : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+          
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/JPaintEditor.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+public class JPaintEditor : EditorWindow
+{
+    private const string jPaintName = "JPaint";
+    private const string jCanvasCamera = "JCamera";
+    private const string jCanvasPlane = "JPlane";
+
+    [MenuItem("JLibrary/JPaint")]
+    public static void Paint()
+    {
+        if (FindJPaint == null)
+        {
+            GameObject main = CreateObj(jPaintName);
+
+            GameObject camera = CreateObj(jCanvasCamera);
+            Camera c = camera.AddComponent<Camera>();
+            c.clearFlags = CameraClearFlags.Depth;
+            c.orthographic = true;
+            c.farClipPlane = 5;
+            c.orthographicSize = 0.5f;
+            SetParent(main,camera);
+
+            GameObject plane = CreateObjPrimitive(jCanvasPlane, PrimitiveType.Quad);
+            plane.transform.position = new Vector3(0, 0, 5);
+            SetParent(main, plane);
+
+        }
+
+    }
+    private static GameObject CreateObj(string objName) => new GameObject(objName);
+    public static void SetParent(GameObject parent, GameObject obj) => obj.transform.SetParent(parent.transform);
+    private static GameObject FindJPaint { get { return GameObject.Find(jPaintName); } }
+    private static GameObject CreateObjPrimitive(string objName, PrimitiveType p)
+    {
+        GameObject primitive = GameObject.CreatePrimitive(p);
+        primitive.name = objName;
+        return primitive;
+    }
+   
+
+    
+
+
+
+}
+#endif
